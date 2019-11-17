81fd4ff6e5327120984a967601a3f3982f943317
Samuel Rosas-Meza <rosasmez@usc.edu>
2019-11-17 14:35:26 -0800

    Revert "Trying to resolve merge conflicts."
    This reverts commit adf12f1ef6c5b37a62f0dda6a6d20eb412d6b262.

---
+++ AsIAm/AsIAm/AsIAm/HomeViewController.swift
@@ -0,0 +1,85 @@
+//
+//  HomeViewController.swift
+//  AsIAm
+//
+//  Created by Martin Anquetil on 9/26/19.
+//  Copyright © 2019 Anup Sankarraman. All rights reserved.
+//
+
+import UIKit
+
+struct myVariables
+   {
+       static var userName = "";
+   }
+class HomeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+   
+    
+    @IBOutlet weak var pickerSelection: UIPickerView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for user in users{
+            model.append(user.name)
+        }
+        if model.count == 0{
+            // create main storyboard instance
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            // from main storyboard, instantiate success view
+            let successVC = storyboard.instantiateViewController(withIdentifier: "NameViewController")
+            
+            // get the app delegate
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            // set the success view controller as root view controller
+            appDelegate.window?.rootViewController = successVC
+        }
+        self.pickerSelection.delegate = self
+        self.pickerSelection.dataSource = self
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    let users = UsersModel.shared.model // TODO: GET NAMES FROM PLIST
+    var model: [String] = [];
+    
+    weak var button: UIButton?
+    var handler: (() -> ())?
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return model.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        return self.model[row]
+    }
+
+    
+    @IBAction func continueButton(_ sender: Any) {
+        var selectedName = model[pickerSelection.selectedRow(inComponent: 0)] //gets name selected to put in plist
+        UsersModel.shared.selectedIndex = pickerSelection.selectedRow(inComponent: 0)
+       print(selectedName)
+        myVariables.userName = selectedName
+        print(myVariables.userName)
+        
+        self.performSegue(withIdentifier: "nameSelectedSegue", sender: self)
+        
+    }
+    
+}
---
+++ AsIAm/AsIAm/AsIAm/TopMenuViewController.swift
@@ -0,0 +1,65 @@
+//
+//  TopMenuViewController.swift
+//  AsIAm
+//
+//  Created by Anup Sankarraman on 9/19/19.
+//  Copyright © 2019 Anup Sankarraman. All rights reserved.
+//
+
+import UIKit
+
+class TopMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        intamacyCircle.layer.masksToBounds = true
+        intamacyCircle.layer.cornerRadius = intamacyCircle.bounds.width / 2
+        
+        sexualIdentityCircle.layer.masksToBounds = true
+        sexualIdentityCircle.layer.cornerRadius = sexualIdentityCircle.bounds.width/2
+        
+        powerCircle.layer.masksToBounds = true
+        powerCircle.layer.cornerRadius = powerCircle.bounds.width/2
+        
+        sensualityCircle.layer.masksToBounds = true
+        sensualityCircle.layer.cornerRadius = sensualityCircle.bounds.width/2
+        
+        sexualHealthCircle.layer.masksToBounds = true
+        sexualHealthCircle.layer.cornerRadius = sexualHealthCircle.bounds.width/2
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //All outlets
+    @IBOutlet weak var intamacyCircle: UIButton!
+    @IBOutlet weak var sexualIdentityCircle: UIButton!
+    @IBOutlet weak var powerCircle: UIButton!
+    @IBOutlet weak var sensualityCircle: UIButton!
+    @IBOutlet weak var sexualHealthCircle: UIButton!
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if (segue.identifier == "healthSegue"){
+            ModuleNavModel.shared.selectedModule = 0
+        }
+        if (segue.identifier == "powerSegue"){
+            ModuleNavModel.shared.selectedModule = 1
+        }
+        if (segue.identifier == "identSegue"){
+            ModuleNavModel.shared.selectedModule = 2
+        }
+        if (segue.identifier == "sensuSegue"){
+            ModuleNavModel.shared.selectedModule = 3
+        }
+        if (segue.identifier == "intimSegue"){
+            ModuleNavModel.shared.selectedModule = 4
+        }
+    }
+
+}
